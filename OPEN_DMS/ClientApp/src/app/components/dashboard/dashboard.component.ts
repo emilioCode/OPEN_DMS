9a980708631b86d75361bc51c4f2de787ef89873
Manuel E Mendez <emilio_mem@hotmail.com>
2022-02-02 23:27:59 -0400

    -Reactor some elements in the front-end -AuthGuard

--- OPEN_DMS/ClientApp/src/app/components/dashboard/dashboard.component.ts
+++ OPEN_DMS/ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/app/classes/user';
+import { CONSTANT } from 'src/app/enums/CONSTANT';
 import { CommonService, ModalMessageService } from 'src/app/services/index';
 import { UserDialogComponent } from './user/user-dialog/user-dialog.component';
 
@@ -12,6 +13,14 @@ import { UserDialogComponent } from './user/user-dialog/user-dialog.component';
 export class DashboardComponent implements OnInit {
   session: any;
   self: User;
+  moduleList: any = [
+    { name: 'Dashboard', class: 'primary', icon: 'dashboard', routerLink: '/dashboard', everyone: true },
+    { name: 'Entity', class: 'primary', icon: 'group_work', routerLink: '/dashboard/entity', everyone: false },
+    { name: 'Teams', class: 'primary', icon: 'folder_shared', routerLink: '/dashboard/team', everyone: false },
+    { name: 'Manage Accounts', class: 'primary', icon: 'manage_accounts', routerLink: '/dashboard/user', everyone: false },
+    { name: 'File Group', class: 'primary', icon: 'folder', routerLink: '/dashboard/file-group', everyone: true }
+  ];
+  modules: any=[];
 
   constructor(
     private commonService: CommonService,
@@ -23,6 +32,9 @@ export class DashboardComponent implements OnInit {
     this.session = this.commonService.sessionStorage.get("user");
     this.self = new User(this.session.id,this.session.name,null,this.session.userName,null,this.session.teamId,
       this.session.entityId, this.session.accessLevel,new Date(),new Date(this.session.expirationDate), false);
+
+      const isAdministrator = this.session.accessLevel == CONSTANT.ROOT || this.session.accessLevel == CONSTANT.ADMINISTRATOR;
+      this.modules = isAdministrator? this.moduleList: this.moduleList.filter(m => m.everyone == true);
   }
 
   logOut = () => {
