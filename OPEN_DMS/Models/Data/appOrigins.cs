0e57cfc743d9d419f96152affdfcf867fb490d79
emilioCode <emilio_mem@hotmail.com>
2022-02-10 19:56:18 -0400

    * appOrigins.json: * OPEN_DMS.csproj: * appOrigins.cs: * FileController.cs:
    * Startup.cs: 1. Document test page to test the service to consume.
    2. Available to be installed in Windows as equals as Unix.

---
+++ OPEN_DMS/Models/Data/appOrigins.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OPEN_DMS.Models.Data
+{
+    public class appOrigins
+    {
+        public string[] AllowedOrigins { get; set; }
+    }
+}
